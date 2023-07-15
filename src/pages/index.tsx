b4740217a12aac4fe1368b8799ad1604ea52c1b7
HabibMollah <hrmollah2002@gmail.com>
2023-07-16 00:49:48 +0600

    remove unused style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="text-center">
       <Link
-        className="mt-60 block bg-gradient-to-r from-[#CA16E7] to-[#FE4754] bg-clip-text p-2 text-center text-6xl font-bold text-transparent hover:underline"
+        className="mt-60 inline-block bg-gradient-to-r from-[#CA16E7] to-[#FE4754] bg-clip-text p-2 text-center text-6xl font-bold text-transparent"
         href="/login">
         Click to go to the login page
       </Link>
