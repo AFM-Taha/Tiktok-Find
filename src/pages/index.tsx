0a3884d25be0e2b71b75893b157056c2182b51da
HabibMollah <hrmollah2002@gmail.com>
2023-07-16 00:28:28 +0600

    install prettier plugin for tailwindcss

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="text-center">
       <Link
-        className="text-center hover:underline mt-60 block bg-gradient-to-r from-[#CA16E7] to-[#FE4754] bg-clip-text text-transparent text-6xl p-2 font-bold"
+        className="mt-60 block bg-gradient-to-r from-[#CA16E7] to-[#FE4754] bg-clip-text p-2 text-center text-6xl font-bold text-transparent hover:underline"
         href="/login">
         Click to go to the login page
       </Link>
