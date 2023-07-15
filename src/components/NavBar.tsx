24d83c3c2af74ac507e145087147e8ce96cc430a
HabibMollah <hrmollah2002@gmail.com>
2023-07-16 04:34:15 +0600

    add buttons to navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,3 +1,24 @@
 export default function NavBar() {
-  return <div className="font-bold text-red-500">NavBar</div>;
+  return (
+    <div className="flex items-center justify-between">
+      <div>LOGO</div>
+      <div className="flex gap-6">
+        <div>
+          <button>Extension</button>
+        </div>
+        <div>
+          <button>Swipes</button>
+        </div>
+        <div>
+          <button>Tools</button>
+        </div>
+        <div>
+          <button>Sign In</button>
+        </div>
+        <div>
+          <button>Try Thieve Free</button>
+        </div>
+      </div>
+    </div>
+  );
 }
