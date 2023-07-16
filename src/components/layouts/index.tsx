2b54edcb338745dc0be1b1e72b50ca698d0312a4
tahamah <taha.iu.bd@gmail.com>
2023-07-16 08:52:10 +0600

    layout change

---
+++ src/components/layouts/index.tsx
@@ -0,0 +1,24 @@
+import { ReactNode } from 'react';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+import { useRouter } from "next/router";
+import UserLayout from './UserLayout';
+import AdminLayout from './AdminLayout';
+
+export default  function Layout({ children }:{ children: ReactNode }) {
+  const router = useRouter();
+  const pathname = router.pathname;
+
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {pathname.includes("admin") ? (
+        <AdminLayout>{children}</AdminLayout>
+      ) : (
+        <UserLayout>{children}</UserLayout>
+      )}
+    </QueryClientProvider>
+  );
+}
+
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import Layout from '@/components/layout';
+import Layout from '@/components/layouts';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 
