f01c01e4ab0de18ba44775d6d7d0601935fd5bae
HabibMollah <hrmollah2002@gmail.com>
2023-07-16 15:34:48 +0600

    add static form

---
+++ src/components/userComponents/common/forms/LoginForm.tsx
@@ -0,0 +1,50 @@
+import Link from 'next/link';
+import { FaArrowRight, FaFacebook } from 'react-icons/fa';
+import { FcGoogle } from 'react-icons/fc';
+
+export default function LoginForm() {
+  return (
+    <form>
+      <div className="mb-4 flex justify-evenly">
+        <button className="rounded-xl bg-[#EFF6FB] px-6 py-2 hover:opacity-60">
+          <FcGoogle size={24} />
+        </button>
+        {/* </div>
+      <div> */}
+        <button className="rounded-xl bg-[#EFF6FB] px-6 py-2 hover:opacity-60">
+          <FaFacebook size={24} color="#1877EA" />
+        </button>
+      </div>
+      <p className="mb-4 text-center text-[#00000033]">OR</p>
+      <div>
+        {/* <label htmlFor="email">Email</label> */}
+        <input
+          className="my-2 rounded-2xl bg-[#EFF6FB] px-3 py-4"
+          id="email"
+          type="email"
+          placeholder="Email"
+        />
+      </div>
+      <div>
+        {/* <label htmlFor="password">Password</label> */}
+        <input
+          className="my-2 rounded-2xl bg-[#EFF6FB] px-3 py-4"
+          id="password"
+          type="password"
+          placeholder="Password"
+        />
+      </div>
+      <div className="mx-auto mt-4 flex w-40 items-center justify-center rounded-full bg-[#1469F3] px-4 py-2 font-medium text-white hover:opacity-80">
+        <button className="mr-2" type="submit">
+          Sign In
+        </button>
+        <FaArrowRight />
+      </div>
+      <Link href="/reset-password">
+        <button className="mx-auto mt-3 block font-medium text-[#5082D8] hover:underline">
+          Forgot Password
+        </button>
+      </Link>
+    </form>
+  );
+}
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,15 +1,30 @@
+import LoginForm from '@/components/userComponents/common/forms/LoginForm';
+import Head from 'next/head';
 export default function Login() {
   return (
-    <div>
-      <div className=''>
-        <h2 className='mt-60 inline-block bg-gradient-to-r from-[#46A0E3] to-[#5845DF] bg-clip-text p-2 text-6xl font-extrabold text-transparent'>
-          Hey,
-          <br />
-          welcome back!
-        </h2>
-        <p className='p-2 font-medium'>Sign in to access your account.</p>
+    <>
+      <Head>
+        <title>Login</title>
+        <style>
+          {`
+        body {
+          background-color: #DBEDF8;
+        }`}
+        </style>
+      </Head>
+      <div className="flex justify-evenly">
+        <div>
+          <h2 className="mt-60 inline-block bg-gradient-to-r from-[#46A0E3] to-[#5845DF] bg-clip-text p-2 text-6xl font-extrabold text-transparent">
+            Hey,
+            <br />
+            welcome back!
+          </h2>
+          <p className="p-2 font-medium">Sign in to access your account.</p>
+        </div>
+        <div className="mt-52 rounded-3xl bg-white p-8">
+          <LoginForm />
+        </div>
       </div>
-      <div>{/* TODO: add login form here */}</div>
-    </div>
-  )
+    </>
+  );
 }
