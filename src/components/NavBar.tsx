a6459f2cf1aa86edb006581a5083f9d9f6089175
HabibMollah <hrmollah2002@gmail.com>
2023-07-16 06:09:09 +0600

    finish NavBar styles

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,22 +1,26 @@
 export default function NavBar() {
   return (
-    <div className="mx-6 my-4 flex items-center justify-between">
-      <div>LOGO</div>
-      <div className="flex gap-6">
-        <div>
-          <button>Extension</button>
+    <div className="mx-8 my-4 flex items-center justify-between">
+      <div className="text-xl font-black tracking-widest">LOGO</div>
+      <div className="flex items-center gap-6 text-sm font-medium">
+        <div className="flex gap-6">
+          <div>
+            <button>Extension</button>
+          </div>
+          <div>
+            <button>Swipes</button>
+          </div>
+          <div>
+            <button>Tools</button>
+          </div>
+          <div>
+            <button>Sign In</button>
+          </div>
         </div>
         <div>
-          <button>Swipes</button>
-        </div>
-        <div>
-          <button>Tools</button>
-        </div>
-        <div>
-          <button>Sign In</button>
-        </div>
-        <div>
-          <button>Try Thieve Free</button>
+          <button className="rounded-xl bg-gradient-to-r from-[#283be5] to-[#0093FF] p-2 px-4 text-white">
+            Try Thieve Free
+          </button>
         </div>
       </div>
     </div>
