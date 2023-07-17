43379a7058a7176170f23502ca9ebb9ee0368df3
HabibMollah <hrmollah2002@gmail.com>
2023-07-18 05:55:13 +0600

    improve styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,11 +13,11 @@ export default function Home() {
         </style>
       </Head>
       <div className="text-center">
-        <h2 className="mt-24 px-8 text-center text-3xl font-bold text-white md:text-6xl lg:mt-60 xl:text-7xl 2xl:text-8xl">
+        <h2 className="mt-24 px-8 text-center text-3xl font-bold text-white md:text-6xl lg:mt-60 xl:text-7xl 2xl:text-9xl">
           Discover the Ultimate Shopping Experience Online
         </h2>
-        <p className="mt-4 inline-block bg-gradient-to-r from-[#CA16E7] to-[#FE4754] bg-clip-text p-2 text-xl font-bold tracking-wider text-transparent md:text-3xl xl:text-4xl 2xl:text-5xl">
-          TRUSTED BY OVER 400,000 MEMBERS.
+        <p className="mt-4 inline-block bg-gradient-to-r from-[#CA16E7] to-[#FE4754] bg-clip-text p-2 text-xl font-bold tracking-wider text-transparent md:mt-16 md:text-3xl xl:text-4xl 2xl:text-5xl">
+          TRUSTED BY OVER 400,000 MEMBERS
         </p>
       </div>
     </>
