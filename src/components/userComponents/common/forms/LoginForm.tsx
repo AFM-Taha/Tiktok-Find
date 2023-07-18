e7cfc8ac4d3df501d9f537583d8d832a5ccce88e
HabibMollah <hrmollah2002@gmail.com>
2023-07-18 07:04:49 +0600

    implement form validation

--- src/components/userComponents/common/forms/LoginForm.tsx
+++ src/components/userComponents/common/forms/LoginForm.tsx
@@ -1,10 +1,23 @@
 import Link from 'next/link';
+import { SubmitHandler, useForm } from 'react-hook-form';
 import { FaArrowRight, FaFacebook } from 'react-icons/fa';
 import { FcGoogle } from 'react-icons/fc';
 
+interface FormDataModel {
+  email: string;
+  password: string;
+}
+
 export default function LoginForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormDataModel>();
+  const onSubmit: SubmitHandler<FormDataModel> = (data) => console.log(data);
+
   return (
-    <form>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div className="mb-4 flex justify-evenly">
         <button className="rounded-xl bg-[#EFF6FB] px-6 py-2 hover:opacity-60">
           <FcGoogle size={24} />
@@ -19,6 +32,10 @@ export default function LoginForm() {
       <div>
         {/* <label htmlFor="email">Email</label> */}
         <input
+          {...register('email', {
+            required: true,
+            pattern: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/gi,
+          })}
           className="my-2 w-full rounded-2xl bg-[#EFF6FB] px-3 py-4"
           id="email"
           type="email"
@@ -28,12 +45,23 @@ export default function LoginForm() {
       <div>
         {/* <label htmlFor="password">Password</label> */}
         <input
+          {...register('password', { required: true, minLength: 8 })}
           className="my-2 w-full rounded-2xl bg-[#EFF6FB] px-3 py-4"
           id="password"
           type="password"
           placeholder="Password"
         />
       </div>
+      {errors.email && (
+        <p className="text-center text-sm text-red-500">
+          Please enter a valid email.
+        </p>
+      )}
+      {errors.password && (
+        <p className="text-center text-sm text-red-500">
+          Password must be 8+ characters long.
+        </p>
+      )}
       <div className="mx-auto mt-4 flex w-40 items-center justify-center rounded-full bg-[#1469F3] px-4 py-2 font-medium text-white hover:opacity-80">
         <button className="mr-2" type="submit">
           Sign In
