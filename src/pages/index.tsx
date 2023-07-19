6beaba3310a5ec4984968333bf6638e536698964
HabibMollah <hrmollah2002@gmail.com>
2023-07-19 11:46:50 +0600

    add product grid to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -55,7 +55,7 @@ export default function Home() {
           </div>
         </div>
       </section>
-      <section>
+      <section className="p-8 md:p-10">
         <ProductsGridHome />
       </section>
     </>
