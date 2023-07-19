55ee23028a0e4f93c9f43da06c17e63e47ce043e
HabibMollah <hrmollah2002@gmail.com>
2023-07-19 11:46:11 +0600

    pass props to products grid in home

--- src/components/userComponents/productsGrid/ProductsGridHome.tsx
+++ src/components/userComponents/productsGrid/ProductsGridHome.tsx
@@ -3,26 +3,106 @@ import ProductsGridHomeCard from '../cards/ProductsGridHomeCard';
 export default function ProductsGridHome() {
   return (
     <div className="mx-auto grid max-w-6xl grid-cols-4 gap-8">
-      <ProductsGridHomeCard />
-      <ProductsGridHomeCard />
-      <ProductsGridHomeCard />
-      <ProductsGridHomeCard />
-      <ProductsGridHomeCard />
-      <ProductsGridHomeCard />
-      <ProductsGridHomeCard />
-      <ProductsGridHomeCard />
-      <ProductsGridHomeCard />
-      <ProductsGridHomeCard />
-      <ProductsGridHomeCard />
-      <ProductsGridHomeCard />
-      <ProductsGridHomeCard />
-      <ProductsGridHomeCard />
-      <ProductsGridHomeCard />
-      <ProductsGridHomeCard />
-      <ProductsGridHomeCard />
-      <ProductsGridHomeCard />
-      <ProductsGridHomeCard />
-      <ProductsGridHomeCard />
+      <ProductsGridHomeCard
+        imgsrc="/fancyroom.webp"
+        alttext="fancy room corner lamp"
+        title="Room Corner Lamp"
+      />
+      <ProductsGridHomeCard
+        imgsrc="/balllamp.webp"
+        alttext="ball lamp"
+        title="Ball Lamp"
+      />
+      <ProductsGridHomeCard
+        imgsrc="/biketailled.webp"
+        alttext="bike tail lamp"
+        title="Bike Tail LED"
+      />
+      <ProductsGridHomeCard
+        imgsrc="/carts.webp"
+        alttext="Plastic carts"
+        title="Plastic Carts"
+      />
+      <ProductsGridHomeCard
+        imgsrc="/canemirror.webp"
+        alttext="Bamboo mirror"
+        title="Bamboo Mirror"
+      />
+      <ProductsGridHomeCard
+        imgsrc="/cloudlamp.webp"
+        alttext="cloud shaped neon light"
+        title="Cloud Neon Light"
+      />
+      <ProductsGridHomeCard
+        imgsrc="/coffeeclampspoon.webp"
+        alttext="coffee clamp spoon"
+        title="Coffee Clamp Spoon"
+      />
+      <ProductsGridHomeCard
+        imgsrc="/coffeemug.webp"
+        alttext="coffeemug"
+        title="Coffee Mug"
+      />
+      <ProductsGridHomeCard
+        imgsrc="/hanginglamp.webp"
+        alttext="hanging lamp"
+        title="Hanging Lamp"
+      />
+      <ProductsGridHomeCard
+        imgsrc="/keychain.webp"
+        alttext="fancy keychain"
+        title="Fancy Keychain"
+      />
+      <ProductsGridHomeCard
+        imgsrc="/monitor.webp"
+        alttext="Full HD monitor"
+        title="Full HD monitor"
+      />
+      <ProductsGridHomeCard
+        imgsrc="/neonlights.webp"
+        alttext="hanging neon lights"
+        title="Hanging Neon Lights"
+      />
+      <ProductsGridHomeCard
+        imgsrc="/orangeheadphone.webp"
+        alttext="Orange Headphone"
+        title="Orange Headphone"
+      />
+      <ProductsGridHomeCard
+        imgsrc="/plants.webp"
+        alttext="small plants"
+        title="Small Plants"
+      />
+      <ProductsGridHomeCard
+        imgsrc="/strawcup.webp"
+        alttext="Cup with Straw"
+        title="Cup with Straw"
+      />
+      <ProductsGridHomeCard
+        imgsrc="/tablelamp.webp"
+        alttext="Round table lamp"
+        title="Round Table Lamp"
+      />
+      <ProductsGridHomeCard
+        imgsrc="/thermbottle.webp"
+        alttext="Thermo Bottle"
+        title="Thermo Bottle"
+      />
+      <ProductsGridHomeCard
+        imgsrc="/touchlock.webp"
+        alttext="fingerprint lock"
+        title="Fingerprint Lock"
+      />
+      <ProductsGridHomeCard
+        imgsrc="/whitevase.webp"
+        alttext="White Ceramic Vase"
+        title="White Ceramic Vase"
+      />
+      <ProductsGridHomeCard
+        imgsrc="/wirelesschargelamp.webp"
+        alttext="Wireless Charge Lamp"
+        title="Wireless Charge Lamp"
+      />
     </div>
   );
 }
