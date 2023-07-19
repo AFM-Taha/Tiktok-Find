78cf4f15c8cc9d2a65413614fdee23745e235170
HabibMollah <hrmollah2002@gmail.com>
2023-07-19 12:25:03 +0600

    add zig zag effect

--- src/components/userComponents/cards/ProductsGridHomeCard.tsx
+++ src/components/userComponents/cards/ProductsGridHomeCard.tsx
@@ -11,11 +11,11 @@ export default function ProductsGridHomeCard({
   alttext,
 }: Props) {
   return (
-    <div className="inline-block overflow-hidden rounded-xl bg-neutral-700">
+    <div className="grid overflow-hidden rounded-3xl bg-neutral-700 shadow-[0_0.625rem_1.25rem_rgba(0,0,0,0.1)] even:translate-y-6 md:even:translate-y-0 lg:rounded-3xl lg:shadow-[0_1.5rem_2.5rem_rgba(0,_0,_0,_0.1)] xl:even:translate-y-10">
       <Image
-        layout="intrinsic"
-        width={280}
-        height={280}
+        // layout="intrinsic"
+        width={350}
+        height={350}
         className="block"
         alt={alttext}
         src={imgsrc}
