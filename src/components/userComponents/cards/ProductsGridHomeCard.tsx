4b40eb9382f1ef2ae2ea6581eea2ed765b526f72
HabibMollah <hrmollah2002@gmail.com>
2023-07-19 11:46:30 +0600

    add props

--- src/components/userComponents/cards/ProductsGridHomeCard.tsx
+++ src/components/userComponents/cards/ProductsGridHomeCard.tsx
@@ -1,17 +1,27 @@
 import Image from 'next/image';
-export default function ProductsGridHomeCard() {
+
+interface Props {
+  imgsrc: string;
+  alttext: string;
+  title: string;
+}
+export default function ProductsGridHomeCard({
+  imgsrc,
+  title,
+  alttext,
+}: Props) {
   return (
-    <div className="inline-block overflow-hidden rounded-xl bg-white">
+    <div className="inline-block overflow-hidden rounded-xl bg-neutral-700">
       <Image
         layout="intrinsic"
         width={280}
         height={280}
         className="block"
-        alt="Room Vibe Corner Lamp"
-        src="/fancyroom.webp"
+        alt={alttext}
+        src={imgsrc}
       />
-      <h3 className="py-2 text-center text-xs font-medium">
-        Fancy Room Corner Lamp
+      <h3 className="py-2 text-center text-xs font-medium text-neutral-200">
+        {title}
       </h3>
     </div>
   );
