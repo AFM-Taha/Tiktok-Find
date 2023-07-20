7183bc67f8e445b996a4221687ffe5b4c3d61365
HabibMollah <hrmollah2002@gmail.com>
2023-07-20 19:38:12 +0600

    create products route

---
+++ src/pages/products.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+export default function Products() {
+  return (
+    <>
+      <Head>
+        <style>{`
+    body {
+        background-color: #000;
+    }
+    `}</style>
+      </Head>
+    </>
+  );
+}
