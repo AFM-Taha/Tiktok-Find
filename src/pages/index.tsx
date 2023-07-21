f5b7a5e88d97deb5f89456eae12b992a36a897ff
HabibMollah <hrmollah2002@gmail.com>
2023-07-21 12:38:55 +0600

    add category scrollbar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import CategoryBar from '@/components/userComponents/common/CategoryBar';
 import ProductsGridHome from '@/components/userComponents/productsGrid/ProductsGridHome';
 import Head from 'next/head';
 
@@ -12,53 +13,10 @@ export default function Home() {
           key="desc"
         />
         <meta name="robots" content="index,follow" />
-        {/* <style>
-          {`
-        body {
-          background-image: linear-gradient(to top right, var(--tw-gradient-stops));
-          --tw-gradient-from: #000000 var(--tw-gradient-from-position);
-          --tw-gradient-to: rgb(0 0 0 / 0) var(--tw-gradient-to-position);
-          --tw-gradient-stops: var(--tw-gradient-from), var(--tw-gradient-to);
-          --tw-gradient-to: #1b2047 var(--tw-gradient-to-position);
-        }`}
-        </style> */}
       </Head>
-      {/* <section className="xs:pt-[8rem] bg-white bg-gradient-to-br from-[#130110] to-[#1b2047] pt-[6.25rem] text-center lg:pt-[10rem] 2xl:pt-[10rem]">
-        <div className="text-center">
-          <h2 className="px-8 text-center text-3xl font-bold text-white md:text-6xl xl:text-7xl 2xl:text-9xl">
-            Discover the Ultimate Shopping Experience Online
-          </h2>
-          <h3 className="mt-4 inline-block bg-gradient-to-r from-[#CA16E7] to-[#FE4754] bg-clip-text p-2  text-xl font-bold tracking-wider text-transparent md:mt-16 md:text-3xl xl:text-4xl 2xl:text-5xl">
-            TRUSTED BY OVER 400,000 MEMBERS
-          </h3>
-        </div>
-        <div className="quotes mx-auto grid max-w-[60rem] justify-center gap-[4.375rem] px-5 py-20 text-white md:grid-cols-3 lg:py-[6rem] 2xl:max-w-[80rem] 2xl:py-[6rem]">
-          <div>
-            <div className="mx-auto mb-4 max-w-[18.75rem] text-[0.875rem] font-bold leading-[120%] lg:text-[1rem] 2xl:max-w-[20rem] 2xl:text-xl">
-              &quot;This platform redefined shopping for me&quot;
-            </div>
-            <div className="text-[0.75rem] uppercase leading-none tracking-wider opacity-30">
-              Jane Doe
-            </div>
-          </div>
-          <div>
-            <div className="mx-auto mb-4 max-w-[18.75rem] text-[0.875rem] font-bold leading-[120%] lg:text-[1rem] 2xl:max-w-[20rem] 2xl:text-xl">
-              &quot;A true shopping paradise&quot;
-            </div>
-            <div className="text-[0.75rem] uppercase leading-none tracking-wider opacity-30">
-              J. Rich
-            </div>
-          </div>
-          <div>
-            <div className="mx-auto mb-4 max-w-[18.75rem] text-[0.875rem] font-bold leading-[120%] lg:text-[1rem] 2xl:max-w-[20rem] 2xl:text-xl">
-              &quot;Shopping will never be the same again&quot;
-            </div>
-            <div className="text-[0.75rem] uppercase leading-none tracking-wider opacity-30">
-              D. Zander
-            </div>
-          </div>
-        </div>
-      </section> */}
+      <nav>
+        <CategoryBar />
+      </nav>
       <section className="mt-40">
         <ProductsGridHome />
       </section>
