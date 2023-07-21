8a4613f890fdc71ebbef3398de1990257f17a56c
HabibMollah <hrmollah2002@gmail.com>
2023-07-21 14:47:46 +0600

    fix product cards in grid

--- src/components/userComponents/cards/ProductsGridHomeCard.tsx
+++ src/components/userComponents/cards/ProductsGridHomeCard.tsx
@@ -3,26 +3,30 @@ import Image from 'next/image';
 interface Props {
   imgsrc: string;
   alttext: string;
-  // title: string;
+  title: string;
 }
 export default function ProductsGridHomeCard({
   imgsrc,
-  // title,
+  title,
   alttext,
 }: Props) {
   return (
-    <div className="grid overflow-hidden rounded-3xl bg-neutral-700 shadow-[0_0.625rem_1.25rem_rgba(0,0,0,0.1)] even:translate-y-6 md:even:translate-y-0 lg:rounded-3xl lg:shadow-[0_1.5rem_2.5rem_rgba(0,_0,_0,_0.1)] xl:even:translate-y-10">
-      <Image
-        // layout="intrinsic"
-        width={350}
-        height={350}
-        className="block"
-        alt={alttext}
-        src={imgsrc}
-      />
-      {/* <h3 className="py-2 text-center text-xs font-medium text-neutral-200">
-        {title}
-      </h3> */}
+    <div className="z-10 bg-red-400 bg-transparent bg-opacity-0 shadow-[0_0.625rem_1.25rem_rgba(0,0,0,0.1)] even:translate-y-6 md:even:translate-y-0 lg:rounded-3xl lg:shadow-[0_1.5rem_2.5rem_rgba(0,_0,_0,_0.1)] xl:even:translate-y-10">
+      <div className="bg-transparent bg-opacity-0">
+        <Image
+          // layout="intrinsic"
+          width={350}
+          height={350}
+          className="rounded-3xl bg-transparent bg-opacity-0"
+          alt={alttext}
+          src={imgsrc}
+        />
+      </div>
+      <div className="bg-transparent bg-opacity-0">
+        <h3 className="bg-transparent bg-opacity-0 font-semibold text-neutral-600">
+          {title}
+        </h3>
+      </div>
     </div>
   );
 }
--- src/components/userComponents/productsGrid/ProductsGridHome.tsx
+++ src/components/userComponents/productsGrid/ProductsGridHome.tsx
@@ -2,106 +2,106 @@ import ProductsGridHomeCard from '../cards/ProductsGridHomeCard';
 
 export default function ProductsGridHome() {
   return (
-    <div className="mx-auto grid max-w-[71.25rem] grid-cols-2 gap-3 px-3 md:grid-cols-3 md:gap-5 md:px-5 lg:gap-8 xl:max-w-[93.75rem] xl:grid-cols-4">
+    <div className="mx-auto grid max-w-[71.25rem] grid-cols-2 gap-3 bg-transparent bg-opacity-0 px-3 md:grid-cols-3 md:gap-5 md:px-5 lg:gap-8 xl:max-w-[93.75rem] xl:grid-cols-4">
       <ProductsGridHomeCard
         imgsrc="/fancyroom.webp"
         alttext="fancy room corner lamp"
-        // title="Room Corner Lamp"
+        title="Room Corner Lamp"
       />
       <ProductsGridHomeCard
         imgsrc="/balllamp.webp"
         alttext="ball lamp"
-        // title="Ball Lamp"
+        title="Ball Lamp"
       />
       <ProductsGridHomeCard
         imgsrc="/biketailled.webp"
         alttext="bike tail lamp"
-        // title="Bike Tail LED"
+        title="Bike Tail LED"
       />
       <ProductsGridHomeCard
         imgsrc="/carts.webp"
         alttext="Plastic carts"
-        // title="Plastic Carts"
+        title="Plastic Carts"
       />
       <ProductsGridHomeCard
         imgsrc="/canemirror.webp"
         alttext="Bamboo mirror"
-        // title="Bamboo Mirror"
+        title="Bamboo Mirror"
       />
       <ProductsGridHomeCard
         imgsrc="/cloudlamp.webp"
         alttext="cloud shaped neon light"
-        // title="Cloud Neon Light"
+        title="Cloud Neon Light"
       />
       <ProductsGridHomeCard
         imgsrc="/coffeeclampspoon.webp"
         alttext="coffee clamp spoon"
-        // title="Coffee Clamp Spoon"
+        title="Coffee Clamp Spoon"
       />
       <ProductsGridHomeCard
         imgsrc="/coffeemug.webp"
         alttext="coffeemug"
-        // title="Coffee Mug"
+        title="Coffee Mug"
       />
       <ProductsGridHomeCard
         imgsrc="/hanginglamp.webp"
         alttext="hanging lamp"
-        // title="Hanging Lamp"
+        title="Hanging Lamp"
       />
       <ProductsGridHomeCard
         imgsrc="/keychain.webp"
         alttext="fancy keychain"
-        // title="Fancy Keychain"
+        title="Fancy Keychain"
       />
       <ProductsGridHomeCard
         imgsrc="/monitor.webp"
         alttext="Full HD monitor"
-        // title="Full HD monitor"
+        title="Full HD monitor"
       />
       <ProductsGridHomeCard
         imgsrc="/neonlights.webp"
         alttext="hanging neon lights"
-        // title="Hanging Neon Lights"
+        title="Hanging Neon Lights"
       />
       <ProductsGridHomeCard
         imgsrc="/orangeheadphone.webp"
         alttext="Orange Headphone"
-        // title="Orange Headphone"
+        title="Orange Headphone"
       />
       <ProductsGridHomeCard
         imgsrc="/plants.webp"
         alttext="small plants"
-        // title="Small Plants"
+        title="Small Plants"
       />
       <ProductsGridHomeCard
         imgsrc="/strawcup.webp"
         alttext="Cup with Straw"
-        // title="Cup with Straw"
+        title="Cup with Straw"
       />
       <ProductsGridHomeCard
         imgsrc="/tablelamp.webp"
         alttext="Round table lamp"
-        // title="Round Table Lamp"
+        title="Round Table Lamp"
       />
       <ProductsGridHomeCard
         imgsrc="/thermbottle.webp"
         alttext="Thermo Bottle"
-        // title="Thermo Bottle"
+        title="Thermo Bottle"
       />
       <ProductsGridHomeCard
         imgsrc="/touchlock.webp"
         alttext="fingerprint lock"
-        // title="Fingerprint Lock"
+        title="Fingerprint Lock"
       />
       <ProductsGridHomeCard
         imgsrc="/whitevase.webp"
         alttext="White Ceramic Vase"
-        // title="White Ceramic Vase"
+        title="White Ceramic Vase"
       />
       <ProductsGridHomeCard
         imgsrc="/wirelesschargelamp.webp"
         alttext="Wireless Charge Lamp"
-        // title="Wireless Charge Lamp"
+        title="Wireless Charge Lamp"
       />
     </div>
   );
