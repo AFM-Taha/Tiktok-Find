a9cd2d58974a01c4fe04b020f320afbf216e09ac
HabibMollah <hrmollah2002@gmail.com>
2023-07-21 13:59:05 +0600

    add videobar

---
+++ src/components/userComponents/video/VideoCard.tsx
@@ -0,0 +1,7 @@
+export default function VideoCard() {
+  return (
+    <div className="flex items-center justify-center rounded-xl bg-slate-200 px-20 py-32">
+      <div className="text-xl font-bold">Story</div>
+    </div>
+  );
+}
