f5bfbadb132748e242224f718f8b709d0b83e1fc
HabibMollah <hrmollah2002@gmail.com>
2023-07-22 00:18:33 +0600

    improve styles

--- src/components/userComponents/video/VideoCard.tsx
+++ src/components/userComponents/video/VideoCard.tsx
@@ -1,6 +1,6 @@
 export default function VideoCard() {
   return (
-    <div className="flex items-center justify-center rounded-xl bg-slate-200 px-20 py-32">
+    <div className="flex items-center justify-center rounded-3xl bg-neutral-700 px-28 py-44 text-white">
       <div className="text-xl font-bold">Story</div>
     </div>
   );
