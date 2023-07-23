d9cb02f68df2cd77a0135f6e409e1e4d93c988d0
HabibMollah <hrmollah2002@gmail.com>
2023-07-23 17:07:13 +0600

    add react query devtools

---
+++ src/pages/products/index.tsx
@@ -0,0 +1,9 @@
+export default function Products() {
+  return (
+    <div className="flex items-center justify-center">
+      <h1 className="text-9xl font-extrabold text-white">
+        You are in the product page
+      </h1>
+    </div>
+  );
+}
