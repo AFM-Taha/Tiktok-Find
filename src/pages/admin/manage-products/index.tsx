a2214f79ec694906296345ee6a2012f9100e8097
tahamah <taha.iu.bd@gmail.com>
2023-08-04 07:07:09 +0600

    admin active  route

--- src/pages/admin/manage-products/index.tsx
+++ src/pages/admin/manage-products/index.tsx
@@ -6,68 +6,15 @@ const productManage = () => {
       <h2 className="text-center text-3xl  font-semibold">Manage Products</h2>
       {/* content here */}
 
-      <div className="relative mt-20 overflow-x-auto shadow-md sm:rounded-lg">
-        <table className="w-full text-left text-sm text-gray-500 dark:text-gray-400">
-          <thead className="bg-gray-50 text-xs uppercase text-gray-700 dark:bg-gray-700 dark:text-gray-400">
-            <tr>
-              <th scope="col" className="px-6 py-3">
-                Product name
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Color
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Category
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Price
-              </th>
-              <th scope="col" className="px-6 py-3">
-                <span className="sr-only">Edit</span>
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr className="border-b bg-white hover:bg-gray-50 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-600">
-              <th
-                scope="row"
-                className="whitespace-nowrap px-6 py-4 font-medium text-gray-900 dark:text-white">
-                Apple MacBook Pro 17
-              </th>
-              <td className="px-6 py-4">Silver</td>
-              <td className="px-6 py-4">Laptop</td>
-              <td className="px-6 py-4">$2999</td>
-              <td className="px-6 py-4 text-right">
-                <Link
-                  href="#"
-                  className="font-medium text-blue-600 hover:underline dark:text-blue-500">
-                  Edit
-                </Link>
-              </td>
-            </tr>
-
-            <tr className="bg-white hover:bg-gray-50 dark:bg-gray-800 dark:hover:bg-gray-600">
-              <th
-                scope="row"
-                className="whitespace-nowrap px-6 py-4 font-medium text-gray-900 dark:text-white">
-                Magic Mouse 2
-              </th>
-              <td className="px-6 py-4">Black</td>
-              <td className="px-6 py-4">Accessories</td>
-              <td className="px-6 py-4">$99</td>
-              <td className="px-6 py-4 text-right">
-                <Link
-                  href="#"
-                  className="font-medium text-blue-600 hover:underline dark:text-blue-500">
-                  Edit
-                </Link>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+      <div className=" mt-20 ">
+        <div className="h-[150px] max-w-3xl bg-gray-100">
+          <input type="text" />
+        </div>
       </div>
     </div>
   );
 };
 
 export default productManage;
+
+
