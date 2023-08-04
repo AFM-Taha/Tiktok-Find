fe2f0933db3eea440173f67ef02cbaede0ff9673
HabibMollah <hrmollah2002@gmail.com>
2023-08-04 22:55:03 +0600

    add form submission

--- src/pages/admin/login.tsx
+++ src/pages/admin/login.tsx
@@ -1,4 +1,13 @@
+import { SubmitHandler, useForm } from 'react-hook-form';
+
+interface Password {
+  password: string;
+}
+
 export default function Login() {
+  const { register, handleSubmit } = useForm<Password>();
+
+  const onSubmit: SubmitHandler<Password> = (data) => console.log(data);
   return (
     <div className="px-16 text-center text-white">
       <h2 className="pt-32 text-5xl font-black sm:text-8xl">
@@ -7,11 +16,14 @@ export default function Login() {
       <p className="my-10 text-xl font-bold sm:text-3xl">
         Enter your passkey to gain access to your ship
       </p>
-      <form className="mt-32 flex flex-col items-center justify-center gap-2 sm:flex-row">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="mt-32 flex flex-col items-center justify-center gap-2 sm:flex-row">
         <label htmlFor="admin-password" className="sr-only">
           Admin Password
         </label>
         <input
+          {...register('password', { required: true })}
           id="admin-password"
           className="rounded-xl border-0 text-black"
           type="password"
