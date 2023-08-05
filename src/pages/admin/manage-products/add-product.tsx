7107fb776aa7be51a039f5a458b177b3b5f02290
HabibMollah <hrmollah2002@gmail.com>
2023-08-06 04:46:59 +0600

    remove deprecated code

--- src/pages/admin/manage-products/add-product.tsx
+++ src/pages/admin/manage-products/add-product.tsx
@@ -100,26 +100,24 @@ const AddProduct = () => {
       <h2 className="text-center text-3xl font-semibold">Manage Products</h2>
       {/* Url Section */}
       <div className="mt-20 flex justify-center">
-        <div className="flex h-[150px] w-[800px] items-center justify-center rounded-lg bg-gray-100 px-10 shadow-lg">
-          <input
-            onKeyPress={(e: React.KeyboardEvent<HTMLInputElement>) => {
-              if (e.key === 'Enter') {
-                makeId();
-              }
-            }}
-            onChange={(e) => {
-              setInputUrl(e.target.value);
-            }}
-            type="text"
-            className="h-10 w-full border-2 border-blue-600 text-gray-800"
-            placeholder="URL"
-          />
-          <button
-            onClick={makeId}
-            className="ml-1 border-2 border-red-800 px-8 py-1.5 text-red-800 duration-200 hover:bg-red-800 hover:text-gray-100">
-            Click
-          </button>
-        </div>
+        <form onSubmit={(e) => e.preventDefault()}>
+          <div className="flex h-[150px] w-[800px] items-center justify-center rounded-lg bg-gray-100 px-10 shadow-lg">
+            <input
+              value={inputUrl}
+              onChange={(e) => {
+                setInputUrl(e.target.value);
+              }}
+              type="text"
+              className="h-10 w-full border-2 border-blue-600 text-gray-800"
+              placeholder="URL"
+            />
+            <button
+              onClick={makeId}
+              className="ml-1 border-2 border-red-800 px-8 py-1.5 text-red-800 duration-200 hover:bg-red-800 hover:text-gray-100">
+              Click
+            </button>
+          </div>
+        </form>
       </div>
       {/* Product data Change section */}
       {title && (
