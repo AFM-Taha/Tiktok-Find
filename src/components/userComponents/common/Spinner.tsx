d10db2d5de4ea1a7c23d193a862880a2428a08bb
HabibMollah <hrmollah2002@gmail.com>
2023-08-06 02:20:45 +0600

    improve spinner

--- src/components/userComponents/common/Spinner.tsx
+++ src/components/userComponents/common/Spinner.tsx
@@ -1,7 +1,7 @@
 export default function Spinner() {
   return (
-    <div className="flex h-[100vh] w-[100vw] items-center justify-center">
-      <div className="flex animate-spin items-center justify-center space-x-2">
+    <div className="grid h-[80vh] place-items-center">
+      <div className="flex animate-spin items-center justify-center">
         <div className="h-8 w-8 rounded-full bg-blue-400"></div>
         <div className="h-8 w-8 rounded-full bg-transparent"></div>
         <div className="h-8 w-8 rounded-full bg-blue-400"></div>
