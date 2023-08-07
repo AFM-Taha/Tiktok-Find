5368f3477590f8c927a52a53cdb56e7dfc72b1b3
tahamah <taha.iu.bd@gmail.com>
2023-08-07 14:06:35 +0600

    patch add

--- src/request/patch.ts
+++ src/request/patch.ts
@@ -1,9 +1,19 @@
 import axios from "axios";
+import toast from "react-hot-toast";
+type RefetchFunction = () => void;
 
-export const patch = async (url: string, data: any) => {
+export const patch = async (url: string, data: any, refetch: RefetchFunction, otherFn?: any) => {
     try {
-        const response = await axios.put(url, data);
-        console.log('Response data:', response.data);
+        const res = await axios.patch(url, data);
+
+        if (res?.status === 200) {
+            refetch();
+            toast.success("Updated");
+            if (otherFn) {
+                otherFn()
+            }
+        }
+
     } catch (error) {
         console.error('Error:', error);
     }
