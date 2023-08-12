684fa2e6a2a506e858df60231b09d4fa74e1f770
HabibMollah <hrmollah2002@gmail.com>
2023-08-12 07:41:50 +0600

    add shorts data in api route

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,41 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next';
 
-type Data = {
-  name: string
-}
+type Shorts = {
+  shorts_links: string[];
+};
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<Shorts>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  // Convert the following links to an array
+  // https://www.youtube.com/shorts/ibuyPg12GmM
+  // https://www.youtube.com/shorts/FxJXVS8_0GA
+  // https://www.youtube.com/shorts/IdwzFJuccak
+  // https://www.youtube.com/shorts/YKZoMUWuI8g
+  // https://www.youtube.com/shorts/nNRBsaVTTa8
+  // https://www.youtube.com/shorts/U5gpmFsnoCc
+  // https://www.youtube.com/shorts/gigSqjSZEJQ
+  // https://www.youtube.com/shorts/_eECWDmYxeI
+  // https://www.youtube.com/shorts/IolO9pr4C10
+  // https://www.youtube.com/shorts/DEJMP1452Yo
+  // https://www.youtube.com/shorts/qUuSD1vUl6k
+  // https://www.youtube.com/shorts/n4lc7jfzaMY
+  res.status(200).json({
+    shorts_links: [
+      'https://www.youtube.com/shorts/ibuyPg12GmM',
+      'https://www.youtube.com/shorts/FxJXVS8_0GA',
+      'https://www.youtube.com/shorts/IdwzFJuccak',
+      'https://www.youtube.com/shorts/YKZoMUWuI8g',
+      'https://www.youtube.com/shorts/nNRBsaVTTa8',
+      'https://www.youtube.com/shorts/U5gpmFsnoCc',
+      'https://www.youtube.com/shorts/gigSqjSZEJQ',
+      'https://www.youtube.com/shorts/_eECWDmYxeI',
+      'https://www.youtube.com/shorts/IolO9pr4C10',
+      'https://www.youtube.com/shorts/DEJMP1452Yo',
+      'https://www.youtube.com/shorts/qUuSD1vUl6k',
+      'https://www.youtube.com/shorts/n4lc7jfzaMY',
+    ],
+  });
 }
