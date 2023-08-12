de8d8d0e5ab97c6317324c64d6e2e705387163f8
HabibMollah <hrmollah2002@gmail.com>
2023-08-12 07:42:14 +0600

    increase z-index of spinner

--- src/components/userComponents/common/Spinner.tsx
+++ src/components/userComponents/common/Spinner.tsx
@@ -2,7 +2,7 @@ import { PropagateLoader } from 'react-spinners';
 
 export default function Spinner() {
   return (
-    <div className="habib-spinner">
+    <div className="habib-spinner z-10">
       <PropagateLoader color="#4C7FE1" />
       <span className="sr-only">Loading</span>
     </div>
