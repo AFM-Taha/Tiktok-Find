cafbdac43ae1f6f38daf50272a9d336c9bd99644
HabibMollah <hrmollah2002@gmail.com>
2023-08-12 07:57:22 +0600

    remove unnecessary comments

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -9,19 +9,6 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Shorts>
 ) {
-  // Convert the following links to an array
-  // https://www.youtube.com/shorts/ibuyPg12GmM
-  // https://www.youtube.com/shorts/FxJXVS8_0GA
-  // https://www.youtube.com/shorts/IdwzFJuccak
-  // https://www.youtube.com/shorts/YKZoMUWuI8g
-  // https://www.youtube.com/shorts/nNRBsaVTTa8
-  // https://www.youtube.com/shorts/U5gpmFsnoCc
-  // https://www.youtube.com/shorts/gigSqjSZEJQ
-  // https://www.youtube.com/shorts/_eECWDmYxeI
-  // https://www.youtube.com/shorts/IolO9pr4C10
-  // https://www.youtube.com/shorts/DEJMP1452Yo
-  // https://www.youtube.com/shorts/qUuSD1vUl6k
-  // https://www.youtube.com/shorts/n4lc7jfzaMY
   res.status(200).json({
     shorts_links: [
       'https://www.youtube.com/shorts/ibuyPg12GmM',
