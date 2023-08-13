776306281f81e4d6ff5a7ca8f98a9ad89d911b12
tahamah <taha.iu.bd@gmail.com>
2023-08-13 15:02:42 +0600

    oredr-detail added

--- src/components/adminComponents/manageOrders/OrderTR.tsx
+++ src/components/adminComponents/manageOrders/OrderTR.tsx
@@ -2,26 +2,18 @@ import { remove } from '@/request/delete';
 import { useState } from 'react';
 import { AiOutlineEye } from 'react-icons/ai';
 import { MdDelete } from 'react-icons/md';
-import { Product } from '@/types/products';
 import { patch } from '@/request/patch';
 import { baseURL } from '@/components/assets/url';
-
+import Image from 'next/image';
 
 const OrderTR = ({ order, refetch }: { order: any; refetch: any }) => {
   const [detail, setDetail] = useState(false);
 
-  const {
-    _id,
-    delivery_status,
-    payment_status,
-    total,
-    products,
-  } = order;
+  const { _id, delivery_status, payment_status, total, products } = order;
 
   const [status, setStatus] = useState<string>(delivery_status);
 
-
-  // __________Order Status Change And Delete________________
+  //  __________Order Status Change And Delete_____________  //
 
   const deleteURL = `${baseURL}/orders/${_id}`;
   const editURL = `${baseURL}/orders/${_id}`;
@@ -48,7 +40,6 @@ const OrderTR = ({ order, refetch }: { order: any; refetch: any }) => {
             <option value="delivered">Delivered</option>
           </select>
         </td>
-        {/* <td className="px-6 py-4">{delivery_status}</td> */}
         <td className="px-6 py-4 text-right">
           <div className="flex items-center justify-end gap-3">
             <button
@@ -68,8 +59,64 @@ const OrderTR = ({ order, refetch }: { order: any; refetch: any }) => {
         <tr>
           <td colSpan={5}>
             <section className=" mx-2  bg-gray-100 p-10">
-              {products.map((p: Product) => {
-                return <div key={p._id}>hello</div>;
+              {products.map((p: any) => {
+                const { image, name, quantity, specification,size,color } = p;
+                return (
+                  <ul
+                    className={`${
+                      products.length > 1 ? 'mb-2' : ''
+                    } rounded-md border-2 border-dashed border-blue-600  p-4`}
+                    key={p._id}>
+                    
+                      <li className="flex w-full flex-col items-start sm:flex-row sm:items-center sm:gap-4">
+                        <div className="flex items-center gap-4">
+                          <Image
+                            width={100}
+                            height={100}
+                            src={image}
+                            alt=""
+                            className="h-16 w-16 rounded object-cover"
+                          />
+                          <div>
+                            <h3 className=" lg:text-lg xl:text-xl ">
+                             Title/Name: {name}
+                            </h3>
+                            <dl className="mt-0.5 space-y-px text-[14px] text-opacity-50">
+                              {size?.length > 1 && (
+                                <div>
+                                  <dt className="inline">Size:</dt>
+                                  <dd className="inline"> {size}</dd>
+                                </div>
+                              )}
+                              {color?.length > 1 && (
+                                <div>
+                                  <dt className="inline">Color:</dt>
+                                  <dd className="inline capitalize">
+                                    {' '}
+                                    {color}
+                                  </dd>
+                                </div>
+                              )}
+                              {specification?.length > 1 && (
+                                <div>
+                                  <dt className="inline">Specification:</dt>
+                                  <dd className="inline capitalize">
+                                    {' '}
+                                    {specification}
+                                  </dd>
+                                </div>
+                              )}
+                            </dl>
+                            <p className='mt-1'>
+                              Quantity: {quantity}
+                            </p>
+                          </div>
+                        </div>
+
+                      </li>
+                    
+                  </ul>
+                );
               })}
             </section>
           </td>
