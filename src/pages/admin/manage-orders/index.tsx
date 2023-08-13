4639bcc2b831828a3c0a1ba618c80fbd844d7286
tahamah <taha.iu.bd@gmail.com>
2023-08-13 10:04:59 +0600

    nan

--- src/pages/admin/manage-orders/index.tsx
+++ src/pages/admin/manage-orders/index.tsx
@@ -1,73 +1,74 @@
-import Link from 'next/link';
+import OrderTR from '@/components/adminComponents/manageOrders/OrderTR';
+import { baseURL } from '@/components/assets/url';
+import Spinner from '@/components/userComponents/common/Spinner';
+import { useGetData } from '@/request/getData';
+import { FetchedProducts } from '@/types/products';
 
-const orderManage = () => {
-  return (
-    <div className="text-gray-100">
-      <h2 className="text-center text-3xl  font-semibold">Manage Orders</h2>
-      {/* content here */}
+const OrderManage = () => {
+  const productGetURL = `${baseURL}/orders`;
 
-      <div className="relative mt-20 overflow-x-auto shadow-md sm:rounded-lg">
-        <table className="w-full text-left text-sm text-gray-500 dark:text-gray-400">
-          <thead className="bg-gray-50 text-xs uppercase text-gray-700 dark:bg-gray-700 dark:text-gray-400">
-            <tr>
-              <th scope="col" className="px-6 py-3">
-                Product name
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Color
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Category
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Price
-              </th>
-              <th scope="col" className="px-6 py-3">
-                <span className="sr-only">Edit</span>
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr className="border-b bg-white hover:bg-gray-50 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-600">
-              <th
-                scope="row"
-                className="whitespace-nowrap px-6 py-4 font-medium text-gray-900 dark:text-white">
-                Apple MacBook Pro 17
-              </th>
-              <td className="px-6 py-4">Silver</td>
-              <td className="px-6 py-4">Laptop</td>
-              <td className="px-6 py-4">$2999</td>
-              <td className="px-6 py-4 text-right">
-                <Link
-                  href="#"
-                  className="font-medium text-blue-600 hover:underline dark:text-blue-500">
-                  Edit
-                </Link>
-              </td>
-            </tr>
+  // Product fetch
+  const { data, isLoading, error, refetch } =
+    useGetData<FetchedProducts>(productGetURL);
 
-            <tr className="bg-white hover:bg-gray-50 dark:bg-gray-800 dark:hover:bg-gray-600">
-              <th
-                scope="row"
-                className="whitespace-nowrap px-6 py-4 font-medium text-gray-900 dark:text-white">
-                Magic Mouse 2
-              </th>
-              <td className="px-6 py-4">Black</td>
-              <td className="px-6 py-4">Accessories</td>
-              <td className="px-6 py-4">$99</td>
-              <td className="px-6 py-4 text-right">
-                <Link
-                  href="#"
-                  className="font-medium text-blue-600 hover:underline dark:text-blue-500">
-                  Edit
-                </Link>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+  // Error and Loading
+  if (error) return;
+  if (isLoading) return <Spinner />;
+
+  console.log(data);
+  if (data)
+    return (
+      <div className="text-gray-100">
+        <h2 className="mb-20 text-center text-3xl  font-semibold">
+          Manage Orders
+        </h2>
+        
+
+        <h5 className="mt-4 text-lg ">
+          {' '}
+          Total Products : 
+          {/* {data.data.totalProducts} */}
+        </h5>
+
+        <div className="relative  mt-2 overflow-x-auto shadow-md sm:rounded-lg">
+          <table className="w-full text-left text-sm text-gray-500 dark:text-gray-400">
+            <thead className="bg-gray-50 text-xs uppercase text-gray-700 dark:bg-gray-700 dark:text-gray-400">
+              <tr>
+                <th scope="col" className="px-6 py-3">
+                  Order Id
+                </th>
+                <th scope="col" className="px-6 py-3">
+Payment Status
+                </th>
+                <th scope="col" className="px-6 py-3">
+Delivery Status                </th>
+                <th scope="col" className="px-6 py-3">
+                  Total Price
+                </th>
+                <th scope="col" className="px-6 py-3 text-right">
+                  Action
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((o,i) => (
+                
+                <OrderTR
+                  key={i}
+                  order={o}
+                  refetch={refetch}
+                />
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
-    </div>
+    );
+  return (
+    <p className="text-center text-2xl font-semibold text-red-600">
+      Something went wrong, please try again.
+    </p>
   );
 };
 
-export default orderManage;
+export default OrderManage;
