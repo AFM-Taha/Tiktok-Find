5b524d29553c3b6b0725b27483cc977b6aee09cb
tahamah <taha.iu.bd@gmail.com>
2023-08-13 12:22:32 +0600

    category

--- src/components/userComponents/common/CategoryBar.tsx
+++ src/components/userComponents/common/CategoryBar.tsx
@@ -1,5 +1,10 @@
+import { baseURL } from '@/components/assets/url';
+import { useGetData } from '@/request/getData';
 import { useRef } from 'react';
 import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
+import Spinner from './Spinner';
+import { Product } from '@/types/products';
+import Link from 'next/link';
 
 export default function CategoryBar() {
   const containerRef = useRef<HTMLDivElement>(null);
@@ -10,6 +15,18 @@ export default function CategoryBar() {
   const handleSlideRight = () => {
     if (containerRef.current) containerRef.current.scrollLeft += 500;
   };
+
+  const url = `${baseURL}/products`;
+  const { data, isLoading }: any = useGetData(url);
+
+  if (isLoading) return <Spinner />;
+
+  const products = data.data.result;
+
+  const uniqueCategories: string[] = [
+    ...new Set(products.map((item) => item.category)),
+  ];
+
   return (
     <div className="mt-16 flex justify-center lg:mx-36">
       <div className="hidden md:inline-block">
@@ -22,78 +39,16 @@ export default function CategoryBar() {
       <div
         ref={containerRef}
         className="category-scrollbar flex gap-2 overflow-x-scroll scroll-smooth px-2 pb-2 text-center text-white md:gap-4 lg:gap-8">
-        <div className="rounded-xl bg-[rgba(26,42,59,0.68)] px-4 py-2 font-medium">
-          Category
-        </div>
-        <div className="rounded-xl bg-[rgba(26,42,59,0.68)] px-4 py-2 font-medium">
-          Category
-        </div>
-        <div className="rounded-xl bg-[rgba(26,42,59,0.68)] px-4 py-2 font-medium">
-          Category
-        </div>
-        <div className="rounded-xl bg-[rgba(26,42,59,0.68)] px-4 py-2 font-medium">
-          Category
-        </div>
-        <div className="rounded-xl bg-[rgba(26,42,59,0.68)] px-4 py-2 font-medium">
-          Category
-        </div>
-        <div className="rounded-xl bg-[rgba(26,42,59,0.68)] px-4 py-2 font-medium">
-          Category
-        </div>
-        <div className="rounded-xl bg-[rgba(26,42,59,0.68)] px-4 py-2 font-medium">
-          Category
-        </div>
-        <div className="rounded-xl bg-[rgba(26,42,59,0.68)] px-4 py-2 font-medium">
-          Category
-        </div>
-        <div className="rounded-xl bg-[rgba(26,42,59,0.68)] px-4 py-2 font-medium">
-          Category
-        </div>
-        <div className="rounded-xl bg-[rgba(26,42,59,0.68)] px-4 py-2 font-medium">
-          Category
-        </div>
-        <div className="rounded-xl bg-[rgba(26,42,59,0.68)] px-4 py-2 font-medium">
-          Category
-        </div>
-        <div className="rounded-xl bg-[rgba(26,42,59,0.68)] px-4 py-2 font-medium">
-          Category
-        </div>
-        <div className="rounded-xl bg-[rgba(26,42,59,0.68)] px-4 py-2 font-medium">
-          Category
-        </div>
-        <div className="rounded-xl bg-[rgba(26,42,59,0.68)] px-4 py-2 font-medium">
-          Category
-        </div>
-        <div className="rounded-xl bg-[rgba(26,42,59,0.68)] px-4 py-2 font-medium">
-          Category
-        </div>
-        <div className="rounded-xl bg-[rgba(26,42,59,0.68)] px-4 py-2 font-medium">
-          Category
-        </div>
-        <div className="rounded-xl bg-[rgba(26,42,59,0.68)] px-4 py-2 font-medium">
-          Category
-        </div>
-        <div className="rounded-xl bg-[rgba(26,42,59,0.68)] px-4 py-2 font-medium">
-          Category
-        </div>
-        <div className="rounded-xl bg-[rgba(26,42,59,0.68)] px-4 py-2 font-medium">
-          Category
-        </div>
-        <div className="rounded-xl bg-[rgba(26,42,59,0.68)] px-4 py-2 font-medium">
-          Category
-        </div>
-        <div className="rounded-xl bg-[rgba(26,42,59,0.68)] px-4 py-2 font-medium">
-          Category
-        </div>
-        <div className="rounded-xl bg-[rgba(26,42,59,0.68)] px-4 py-2 font-medium">
-          Category
-        </div>
-        <div className="rounded-xl bg-[rgba(26,42,59,0.68)] px-4 py-2 font-medium">
-          Category
-        </div>
-        <div className="rounded-xl bg-[rgba(26,42,59,0.68)] px-4 py-2 font-medium">
-          Category
-        </div>
+        {uniqueCategories.map((c, i) => {
+          return (
+            <Link
+              href={'/'}
+              key={i}
+              className="rounded-xl bg-[rgba(26,42,59,0.68)] px-4 py-2 font-medium capitalize">
+              {c}
+            </Link>
+          );
+        })}
       </div>
       <div className="hidden md:inline-block">
         <button
