1e680329e64899b4c686550d8108d657bad6496c
HabibMollah <hrmollah2002@gmail.com>
2023-08-16 01:18:09 +0600

    improve category products page responsiveness

--- src/pages/products/category-products/[categoryName].tsx
+++ src/pages/products/category-products/[categoryName].tsx
@@ -39,26 +39,33 @@ const CategoryProducts = () => {
             },
           ]}
         />
-        <div className="mx-auto grid max-w-[71.25rem] grid-cols-2 gap-3 bg-transparent bg-opacity-0 px-3 md:grid-cols-3 md:gap-5 md:px-5 lg:gap-8 xl:max-w-[93.75rem] xl:grid-cols-4">
-          {products.map((p: Product, i: number) => {
-            const { _id, title, orginal_price, price, item_imgs } = p;
+        <section className="mt-8 min-h-[3000px] pb-[6.25rem]">
+          <div className="py-20">
+            <div className="mx-auto grid max-w-[71.25rem] grid-cols-2 gap-3 bg-transparent bg-opacity-0 px-3 md:grid-cols-3 md:gap-5 md:px-5  lg:gap-8 xl:max-w-[93.75rem] xl:grid-cols-4">
+              {products.map((p: Product, i: number) => {
+                const { _id, title, orginal_price, price, item_imgs } = p;
 
-            return (
-              <Link
-                className="md:even:translate-y-8"
-                key={i}
-                href={`/products/single-products/${_id}`}>
-                <ProductsGridCard
-                  basePrice={Number(orginal_price)}
-                  currentPrice={Number(price)}
-                  imgsrc={item_imgs[0].url}
-                  alttext="fancy room corner lamp"
-                  title={title}
-                />
-              </Link>
-            );
-          })}
-        </div>
+                return (
+                  <div
+                    key={p._id}
+                    className="even:translate-y-6 md:even:translate-y-0 lg:rounded-3xl xl:even:translate-y-10">
+                    <Link
+                      className="md:even:translate-y-8"
+                      href={`/products/single-products/${_id}`}>
+                      <ProductsGridCard
+                        basePrice={Number(orginal_price)}
+                        currentPrice={Number(price)}
+                        imgsrc={item_imgs[0].url}
+                        alttext="fancy room corner lamp"
+                        title={title}
+                      />
+                    </Link>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </section>
       </div>
     </div>
   );
